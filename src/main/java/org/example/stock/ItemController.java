21d37889718b6d0ce390c083771169a3986e34b6
YouCanCallMeGus <gustavosantanas.2019@gmail.com>
2025-03-13 17:32:48 -0300

    Item CRUD

--- src/main/java/org/example/stock/ItemController.java
+++ src/main/java/org/example/stock/ItemController.java
@@ -1,4 +1,41 @@
 package org.example.stock;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
 public class ItemController {
+
+    @Autowired
+    private ItemService itemservice;
+
+    @GetMapping("/item")
+    public HashMap<Integer, Item> getItens() {
+        return itemservice.getItens();
+    }
+
+    @GetMapping("/item/{id}")
+    public Item getItem(@PathVariable int id) {
+        return itemservice.getItem(id);
+    }
+
+    @PostMapping("/item")
+    public String postItem(@RequestBody Item item) {
+        itemservice.postItem(item);
+        return "";
+    }
+
+    @PutMapping("/item/{id}")
+    public String putItem(@PathVariable int id) {
+        itemservice.putItem(id);
+        return "";
+    }
+
+    @DeleteMapping("/item/{id}")
+    public String deleteItem(@PathVariable int id) {
+        itemservice.deleteItem(id);
+        return "";
+    }
 }
--- src/main/java/org/example/stock/ItemService.java
+++ src/main/java/org/example/stock/ItemService.java
@@ -3,6 +3,35 @@ package org.example.stock;
 import java.util.HashMap;
 
 public class ItemService {
-    private HashMap<String, Item> stock;
+    private HashMap<Integer, Item> stock;
+
+    public HashMap<Integer, Item> getItens() {
+        return stock;
+    }
+
+    public Item getItem(int id) {
+        for (Item item: stock.values()) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public void postItem(Item item) {
+        stock.put(item.getId(), item);
+    }
+
+    public void deleteItem(int id) {
+        for (Item v: stock.values()) {
+            if (id == v.getId()) {
+                stock.remove(v);
+            }
+        }
+    }
+
+    public void putItem(int id) {
+
+    }
 
 }
